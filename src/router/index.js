8303d1bdcabef65f2693d01399d1a8a7aae59c99
glebkiselev <12bny23gleb@gmail.com>
2020-03-31 14:38:28 +0300

    on prettier

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home'),
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home")
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('@/views/Login'),
-  },
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login")
+  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
